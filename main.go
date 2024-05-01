e0b2ea62c3af1f1fcacdd8a534b52fede1b94295
Pangolecimal <domkuzaleza@gmail.com>
2024-05-02 01:11:59 +0300

    90% done with home page

--- main.go
+++ main.go
@@ -6,16 +6,26 @@ import (
 	"net/http"
 )
 
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Request received: %s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+		log.Println("Request handled successfully")
+	})
+}
+
 func main() {
 
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("views/index.html"))
-		tmpl.Execute(w, nil)
-	})
+	http.Handle("/", loggingMiddleware(http.HandlerFunc(handler)))
 
 	log.Print("Listening on :42069")
 	log.Fatal(http.ListenAndServe(":42069", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("views/index.html"))
+	tmpl.Execute(w, nil)
 }
