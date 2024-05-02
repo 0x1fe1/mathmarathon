f35ac61670bfed4117a4b7108e6ebe7903574161
Pangolecimal <domkuzaleza@gmail.com>
2024-05-03 01:55:41 +0300

    added /mathmarathon and done with theme switch

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -19,13 +20,32 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	http.Handle("/", loggingMiddleware(http.HandlerFunc(handler)))
+	http.Handle("/", loggingMiddleware(http.HandlerFunc(handle_index)))
+	http.Handle("/mathmarathon", loggingMiddleware(http.HandlerFunc(handle_mathmarathon)))
+
+	http.Handle("/signin", loggingMiddleware(http.HandlerFunc(handle_signin)))
 
 	log.Print("Listening on :42069")
 	log.Fatal(http.ListenAndServe(":42069", nil))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handle_index(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("views/index.html"))
 	tmpl.Execute(w, nil)
+}
+
+func handle_mathmarathon(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("views/mathmarathon.html"))
+	tmpl.Execute(w, nil)
+}
+
+var account_state = "Sign In"
+
+func handle_signin(w http.ResponseWriter, r *http.Request) {
+	if account_state == "Sign In" {
+		account_state = "Log Out"
+	} else {
+		account_state = "Sign In"
+	}
+	fmt.Fprintf(w, account_state)
 }
